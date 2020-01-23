79d895804705de63e782d7b8bc4b582d2dfd4e2c
Aman Sinha <sinha.aman@snapdeal.com>
2020-01-23 13:45:29 +0530

    index

---
+++ src/main/java/com/snapdeal/web/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.snapdeal.web.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.jsp";
+    }
+
+}
