8cf08653ea5b05ca3758764bb6907d12155bc17d
sanu.gupta <sanu.gupta@snapdeal.com>
2020-01-23 15:16:17 +0530

    Sanu

--- src/main/java/com/snapdeal/web/controller/Controller.java
+++ src/main/java/com/snapdeal/web/controller/Controller.java
@@ -18,7 +18,26 @@ public class Controller {
     }
 
     @PostMapping("compareTwoPoints")
-    public void compareTwoPoints(GeoPointSRO point1, GeoPointSRO point2) {
+    public double compareTwoPoints(GeoPointSRO point1, GeoPointSRO point2) {
+        int R= 6371;
+        double lat1 =point1.getLattitude().getAngle();
+       double lat2 =point2.getLattitude().getAngle();
+       double lon1 =point1.getLongitude().getAngle();
+       double lon2 =point2.getLongitude().getAngle();
 
+      double distanceLat = deg2rad(lat2-lat1);
+      double distanceLon = deg2rad(lon2-lon1);
+
+     double a= Math.sin(distanceLat/2) * Math.sin(distanceLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))* Math.sin(distanceLon/2) * Math.sin(distanceLon/2);
+     double c = 2* Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+     double d = R*c;
+     return d;
+
+    }
+
+
+
+    private double deg2rad(double degree){
+         return degree * (Math.PI/180);
     }
 }
