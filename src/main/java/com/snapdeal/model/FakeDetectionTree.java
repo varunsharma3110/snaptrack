2e4452a6cb01624930ef9b586885b87f9c82cf21
sanu.gupta <sanu.gupta@snapdeal.com>
2020-01-23 13:24:11 +0530

    Sanu1

---
+++ src/main/java/com/snapdeal/model/FakeDetectionTree.java
@@ -0,0 +1,12 @@
+package com.snapdeal.model;
+
+public class FakeDetectionTree {
+
+    private String rtoReason;
+
+    private boolean otp;
+
+    private boolean location;
+
+    private boolean callDetails;
+}
