6b7fcc8f242db47ee4583b0e600685a5b9158027
VarunSharma3110 <varun.sharma04@snapdeal.com>
2020-01-23 20:58:51 +0530

    added tree api

--- src/main/java/com/snapdeal/web/services/DecisionTreeService.java
+++ src/main/java/com/snapdeal/web/services/DecisionTreeService.java
@@ -2,6 +2,7 @@ package com.snapdeal.web.services;
 
 import com.snapdeal.entity.SnaptrackMasterDecision;
 import com.snapdeal.enums.RTOType;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,9 @@ public class DecisionTreeService {
                     JSONObject child = new JSONObject();
                     child.put("name", "OTP/Yes");
                     child.put("parent", "OTPAvailable");
-                    object.put("children", child);
+                    JSONArray array = new JSONArray();
+                    array.put(child);
+                    object.put("children", array);
                 }
                 else {
                     JSONObject child = new JSONObject();
@@ -29,27 +32,32 @@ public class DecisionTreeService {
                         child = new JSONObject();
                         child.put("name", "getLocation/Yes");
                         child.put("parent", "OTPAvailable");
-                        object.getJSONObject("children").put("children", child);
+                        JSONArray array = new JSONArray();
+                        array.put(child);
+                        object.getJSONArray("children").getJSONObject(0).put("children", array);
                     }
                     else {
                         child = new JSONObject();
                         child.put("name", "getLocation/No");
                         child.put("parent", "OTPAvailable");
-                        object.getJSONObject("children").put("children", child);
+                        JSONArray array = new JSONArray();
+                        array.put(child);
+                        object.getJSONArray("children").getJSONObject(0).put("children", array);
                         if (decision.getCall_validated()) {
-                            child = new JSONObject();
-                            child.put("name", "callStatus/Yes");
-                            child.put("parent", "getLocation/No");
-                            object.getJSONObject("children").getJSONObject("children").put("name", "Yellow zone");
+//                            child = new JSONObject();
+//                            child.put("name", "callStatus/Yes");
+//                            child.put("parent", "getLocation/No");
+                            object.getJSONArray("children").getJSONObject(0).getJSONArray("children").getJSONObject(0).put("name", "Yellow zone");
                         }
                         else {
-                            child = new JSONObject();
-                            child.put("name", "callStatus/No");
-                            child.put("parent", "getLocation/No");
-                            object.getJSONObject("children").getJSONObject("children").put("name", "Fake");
+//                            child = new JSONObject();
+//                            child.put("name", "callStatus/No");
+//                            child.put("parent", "getLocation/No");
+                            object.getJSONArray("children").getJSONObject(0).getJSONArray("children").getJSONObject(0).put("name", "Fake");
                         }
                     }
                 }
+                break;
             case CNA:
                 object.put("name", "getLocation/Yes");
                 object.put("parent", "null");
